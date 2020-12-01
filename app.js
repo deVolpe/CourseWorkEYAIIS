433ab79b7f8094be642e6ae8fc584f1c94100786
Lel <lel@lel.com>
2020-12-02 00:57:21 +0300

    init

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(require('cors'));
 
-app.get('/api/routes', (req, res) => {});
+app.get('/api/routes', (req, res) => {
+	console.log(req.query);
+	console.log(req.path);
+	console.log(req.body);
+	res.json({}).end();
+});
 
 app.listen(port, () => console.log(`Server has been started ${port}`));
