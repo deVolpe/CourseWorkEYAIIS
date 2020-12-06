37faece35c7fd6c7a66f2e45f706145829670600
Vladimir <successcraftlabtasks@gmail.com>
2020-12-06 21:03:10 +0300

    added: root route

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
+app.get('/', (req, res) => {
+	res.redirect('/api/routes/timetable');
+});
+
 app.use('/api/routes', timetableRoutes);
 
 app.listen(port, () => console.log(`Server has been started ${port}`));
